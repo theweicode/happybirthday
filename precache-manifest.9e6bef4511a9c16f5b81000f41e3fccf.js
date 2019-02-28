956ef6e7284d37b5cbf67dc1c40dfb2a9c21e756
theweicode <theweicode@gmail.com>
2019-02-28 13:12:10 -0800

    Updates

---
+++ precache-manifest.9e6bef4511a9c16f5b81000f41e3fccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6988c18f6c4b3f0b8a1",
+    "url": "/happybirthday/static/js/runtime~main.a6988c18.js"
+  },
+  {
+    "revision": "476b8c76fc80fc2ed91e",
+    "url": "/happybirthday/static/js/main.476b8c76.chunk.js"
+  },
+  {
+    "revision": "fd4751d05dbcae878342",
+    "url": "/happybirthday/static/js/2.fd4751d0.chunk.js"
+  },
+  {
+    "revision": "476b8c76fc80fc2ed91e",
+    "url": "/happybirthday/static/css/main.f0e2c1fd.chunk.css"
+  },
+  {
+    "revision": "fd4751d05dbcae878342",
+    "url": "/happybirthday/static/css/2.ecf7bc33.chunk.css"
+  },
+  {
+    "revision": "922ca7834196815815017889c0ff9d99",
+    "url": "/happybirthday/index.html"
+  }
+];
